db8f3dc3769a0de9719b98e2535d7c128367d271
nujisato <124652784+nujisato@users.noreply.github.com>
2024-12-29 21:32:38 +0900

    Add files via upload

---
+++ Planaria/preload.js
@@ -0,0 +1,9 @@
+// preload.js: Electron Preload Script
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  openFile: () => ipcRenderer.invoke('dialog:openFile'),
+  saveAsFile: (content) => ipcRenderer.invoke('dialog:saveAsFile', content),
+  saveFile: (filePath, content) => ipcRenderer.invoke('dialog:saveFile', { filePath, content }),
+  on: (channel, callback) => ipcRenderer.on(channel, callback), // メッセージリスナー
+});
