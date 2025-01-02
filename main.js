1bfcb239f1f3cead96deea23641910447cb0ec69
nujisato <124652784+nujisato@users.noreply.github.com>
2025-01-02 17:07:34 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app,  BrowserWindow, ipcMain, dialog, shell, menu} = require('electron');
+const { app,  BrowserWindow, ipcMain, dialog, shell,Menu} = require('electron');
 const path = require('path');
 const fs = require('fs');
 let mainWindow;
@@ -8,41 +8,70 @@ app.on('ready', () => {
     width: 1200,
     height: 800,
     webPreferences: {
-      devTools: true , //デベロッパーツールの起動を許可する
+      devTools: true ,
       contextIsolation: true,
-      enableRemoteModule: true, //remoteを有効にするため
-      nodeIntegration: true,
+      enableRemoteModule: false, 
+      nodeIntegration: false,
       preload: path.join(__dirname, 'preload.js'),
     },
   });
-  mainWindow.setMenuBarVisibility(true); // メニューを表示するか
+  mainWindow.setMenuBarVisibility(false); // メニューを表示するか
   mainWindow.loadFile('index.html');// メインウィンドウに表示するURL
+
+  //メニューを設定
+const menu = Menu.buildFromTemplate([
+    { role: 'copy' },
+    { role: 'paste' },
+    { role: 'cut' },
+    { role: 'delete' },
+    { type: 'separator' },
+    { role: 'selectAll' },
+    { type: 'separator' },
+    {
+      label: '表示',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forcereload' },
+        { role: 'toggledevtools' },
+        { type: 'separator' },
+        { role: 'resetzoom' },
+        { role: 'zoomin' },
+        { role: 'zoomout' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' },
+      ],
+    },
+  ]);
+
+  Menu.setApplicationMenu(menu); // メニューバーとして設定
+  // 標準のコンテキストメニューを右クリックに適用
+  mainWindow.webContents.on('context-menu', () => {
+    menu.popup({ window: mainWindow });
+  });
 
   // 外部リンクを標準ブラウザで開く
   mainWindow.webContents.setWindowOpenHandler(({ url }) => {
     if (url.startsWith('http')) {
-      shell.openExternal(url); // 標準ブラウザでURLを開く
-      return { action: 'deny' }; // Electronで新しいウィンドウを開かない
+      shell.openExternal(url);
+      return { action: 'deny' }; 
     }
     return { action: 'allow' };
   });
 });
 
-
 // 全てのウィンドウが閉じたときの処理
 app.on("close", (event) => {
   // macOSのとき以外はアプリを終了させます
   if (process.platform !== "darwin") {
-    // アプリ終了
     app.quit();
   }
 });
 
-//ファイルを開く
+//Open file
 ipcMain.handle('dialog:openFile', async () => {
   const { canceled, filePaths } = await dialog.showOpenDialog(mainWindow, {
     properties: ['openFile'],
-    filters: [{ name: 'text', extensions: ['md','txt','html','css','js'] }],
+    filters: [{ name: 'text', extensions: ['md','txt','markdown'] }],
   });
 
   if (!canceled && filePaths.length > 0) {
@@ -52,12 +81,12 @@ ipcMain.handle('dialog:openFile', async () => {
   return null;
 });
 
-// ファイルを上書き保存する処理
+// Save
 ipcMain.handle('dialog:saveFile', async (_, { filePath, content }) => {
   fs.writeFileSync(filePath, content, 'utf-8');
 });
 
-//ファイルを保存
+//Save As
 ipcMain.handle('dialog:saveAsFile', async (_, content) => {
   const { canceled, filePath } = await dialog.showSaveDialog(mainWindow, {
     filters: [{ name: 'Markdown', extensions: ["txt", "html", "md", "css", "js"] }],
